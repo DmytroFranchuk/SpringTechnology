0573972fbc5d0270d4c3486784723e645a2ac359
Dmytro <franchuk.df@gmail.com>
2024-03-07 14:49:04 +0100

    first commit

---
+++ SpringTechnologyBankApp/src/main/java/de/telran/SpringTechnologyBankApp/SpringTechnologyBankAppApplication.java
@@ -0,0 +1,13 @@
+package de.telran.SpringTechnologyBankApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTechnologyBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTechnologyBankAppApplication.class, args);
+	}
+
+}
